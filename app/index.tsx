d035349c8dcffb50444f50fb8ac843106f0393cb
sawan1k <sawankushwaha222@gmail.com>
2024-04-05 11:47:43 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Page = () => {
+  return (
+    <View>
+      <Text>FIrst Page</Text>
+    </View>
+  );
+};
+
+export default Page;
